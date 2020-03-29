b61946a76102a6d2f316ac8b8613367e0ea1255e
Danny Ferdinandusz <dannyferdinandusz@dannys-imac.home>
2020-03-30 10:47:11 +1100

    async/promise

---
+++ index.js
@@ -0,0 +1,69 @@
+console.log('Before');
+getUser(1, (user) => {
+  getRepositories(user.gitHubUsername, (repos) => {
+    getCommits(repos[0], (commits) => {
+      console.log(commits);
+    })
+  })
+});
+
+//Promise-based
+getUser(1)
+  .then(user => getRepositories(user.gitHubUsername))
+  .then(repos => getCommits(repos[0]))
+  .then(commits => console.log('Commits', commits))
+  .catch(err => console.log('Error', err.message));
+
+//Async-based
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+  const repos = await getRepositories(user.gitHubUsername);
+  const commits = await getCommits[0];
+  console.log(commits);
+  }
+  catch(err) {
+    console.log('Error', err.message);
+  }
+}
+displayCommits();
+
+
+
+
+console.log('After');
+
+
+
+
+
+function getUser(id){
+  return new promise ((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Reading a user from a database');
+      resolve({ id:id, gitHubUsername: 'danny'});
+    }, 2000);
+  });
+}
+  
+function getRepositories(username){
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Calling gitHub API');
+      reject(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+  });
+}
+
+function getCommits(repo){
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Calling gitHub API');
+      resolve(['commit']);
+    }, 2000);
+  });
+}
+
+
+
+  
---
+++ promise-api.js
@@ -0,0 +1,24 @@
+//const p = Promise.resolve({ id: 1 });
+//p.then (result => console.log(result));
+
+//const p = Promise.reject(new Error('reason for rejection'));
+//p.catch(error => console.log(error));
+
+const p1 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log('Async operation 1....');
+    resolve(1);
+    //reject(new Error('because something failed'));
+  }, 2000);
+});
+
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log('Async operation 2....');
+    resolve(2);
+  }, 2000);
+});
+
+Promise.race([p1, p2])
+  .then(result => console.log(result))
+  .catch(err => console.log('Error', err.message));
